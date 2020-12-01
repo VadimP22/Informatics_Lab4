68f322ebf5fa5a81f3e0b82d42982dc639aaa68e
VadimP22 <42093622+VadimP22@users.noreply.github.com>
2020-12-01 12:07:43 +0300

    Add files via upload

--- python/3/lab4_3.py
+++ python/3/lab4_3.py
@@ -4,12 +4,35 @@ import re
 def func(number: int) -> int:
     return 3*(number**2) + 5
 
+
+def is_have_element(list, element):
+    for e in list:
+        if e == element:
+            return True
+
+    return False
+
+
+def remove_repetitions(list):
+
+    new_list = []
+
+    for element in list:
+        if not is_have_element(new_list, element):
+            new_list.append(element)
+
+    return new_list
+        
+
+
 def replace(text_input):
     
     text = str(text_input)
     pattern = re.compile(r"\d+")
 
     match_list = re.findall(pattern, text)
+
+    match_list = remove_repetitions(match_list)
 
     for match in match_list:
         # while match in text:
